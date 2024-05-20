0561b5a61dffa95838ea908bbad1c0be85951470
iota <ishu2000pandey@gmail.com>
2024-05-20 20:46:28 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebiners from "@/components/UpComingWebiners";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+export default function Home() {
+  return(
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <HeroSection />
+    <FeaturedCourses />
+    <WhyChooseUs />
+    <TestimonialCards />
+    <UpcomingWebiners />
+    <Instructors />
+    <Footer />
+    </main>
+    
+  )
+}
